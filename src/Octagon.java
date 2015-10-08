26cdba42f74cc54cc05c47cd174ed74607d8eade
league-workshopper-1 <workshop@Mac-mini.local>
2015-10-07 17:58:55 -0700

    10/7/15

---
+++ src/Octagon.java
@@ -0,0 +1,29 @@
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Octagon {
+	public static void main(String[] args) {
+		int 
+		String answer = JOptionPane.showInputDialog("why shape would you like me to draw");
+		if (answer.equalsIgnoreCase("octagon")) {
+
+			Robot bob = new Robot();
+			bob.penDown();
+			bob.setSpeed(10);
+			for (int i = 0; i < 8; i++) {
+				bob.move(100);
+				bob.turn(360 / 8);
+			}
+		}
+		if (answer.equalsIgnoreCase("triangle")) {
+			Robot bob = new Robot();
+			bob.penDown();
+			bob.setSpeed(10);
+			for (int i = 0; i < 3; i++) {
+				bob.move(100);
+				bob.turn(360 / 3);
+			}
+		}
+	}
+}
---
+++ src/bieberfever.java
@@ -0,0 +1,21 @@
+
+public class bieberfever {
+	public static void main(String[] args) {
+		String word = "baby";
+		String bob = "oh";
+		for (int i = 0; i < 3; i++) {
+			System.out.println(word);
+
+		}
+		System.out.println(bob);
+		for (int i = 0; i < 3; i++) {
+			System.out.println(word);
+		}
+		System.out.println("no");
+		for (int i = 0; i < 3; i++) {
+			System.out.println(word);
+		}
+		System.out.println(bob);
+		System.out.println("i thought you'd always be mine");
+	}
+}
